60a75a2ef945d1544aeebb065b274ccb8141dfac
Biswadeep Mazumder <myselfbiswadeep@gmail.com>
2018-02-25 20:32:43 +0530

    Adding the Cancel AND SEND AGAIN Friend Request Functionality ALONG WITH THE ACCEPT FRIEND REQUEST BUTTON VALUE CHANGING

--- app/src/main/java/android/meraki/speedpost/ProfileActivity.java
+++ app/src/main/java/android/meraki/speedpost/ProfileActivity.java
@@ -26,7 +26,7 @@ public class ProfileActivity extends AppCompatActivity {
 
     private ImageView mProfileImage;
     private TextView mProfileName, mProfileStatus, mProfileFriendsCount;
-    private Button mProfileSendReqButton;
+    private Button mProfileSendReqButton, mProfileDeclineReqButton;
     private DatabaseReference mUserDatabase;
     private DatabaseReference mFriendRequestDatabase;
     private ProgressDialog mProgressbar;
@@ -46,6 +46,7 @@ public class ProfileActivity extends AppCompatActivity {
 
         mProfileImage = findViewById(R.id.profile_image);
         mProfileSendReqButton = findViewById(R.id.profile_send_req_button);
+        mProfileDeclineReqButton = findViewById(R.id.profile_decline_req_button);
         mProfileName = findViewById(R.id.profile_display_name);
         mProfileStatus = findViewById(R.id.profile_status);
         mProfileFriendsCount = findViewById(R.id.profile_totalfriends);
@@ -68,11 +69,43 @@ public class ProfileActivity extends AppCompatActivity {
                 String display_status = dataSnapshot.child("status").getValue().toString();
                 String display_image = dataSnapshot.child("image").getValue().toString();
 
-                mProgressbar.show();
+
                 mProfileName.setText(display_name);
                 mProfileStatus.setText(display_status);
+
                 Picasso.with(ProfileActivity.this).load(display_image).placeholder(R.drawable.profile_pic).into(mProfileImage);
-                mProgressbar.dismiss();
+
+
+
+                //---------- FRIEND LIST / REQUEST FEATURE ----------
+
+                mFriendRequestDatabase.child(mCurrent_User.getUid()).addListenerForSingleValueEvent(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(DataSnapshot dataSnapshot) {
+                        if(dataSnapshot.hasChild(user_Id))
+                        {
+
+                            String request_type = dataSnapshot.child(user_Id).child("request_type").getValue().toString();
+
+                            if(request_type.equals("received"))
+                            {
+
+                                    mState = "request_received";
+                                    mProfileSendReqButton.setText("ACCEPT REQUEST");
+                            }else if(request_type.equals("sent"))
+                            {
+                                    mState = "request_sent";
+                                    mProfileSendReqButton.setText("SEND FRIEND REQUEST");
+                            }
+
+                        }
+                    }
+
+                    @Override
+                    public void onCancelled(DatabaseError databaseError) {
+
+                    }
+                });
 
             }
 
@@ -88,9 +121,20 @@ public class ProfileActivity extends AppCompatActivity {
 
                 mProfileSendReqButton.setEnabled(false);
 
+                mProgressbar.dismiss();
+                //********** HIDING THE BUTTONS SO THAT THE USER CANT SEND FRIEND REQUEST TO HIMSELF/HERSELF*********
+//                if(user_Id.equals(mCurrent_User.getUid()))
+//                {
+//                    mProfileSendReqButton.setVisibility(View.INVISIBLE);
+//                    mProfileDeclineReqButton.setVisibility(View.INVISIBLE);
+//                }
+
+
+
                 //---------------- NOT FRIEND STATE -------------------
                 if(mState.equals("not_friends"))
                 {
+
                     mFriendRequestDatabase.child(mCurrent_User.getUid()).child(user_Id).child("request_type")
                             .setValue("sent").addOnCompleteListener(new OnCompleteListener<Void>() {
                         @Override
@@ -106,7 +150,7 @@ public class ProfileActivity extends AppCompatActivity {
                                             mProfileSendReqButton.setEnabled(true);
                                             mState = "request_sent";
                                             mProfileSendReqButton.setText("Cancel Friend Request");
-                                            Toast.makeText(ProfileActivity.this,"Request Sent",Toast.LENGTH_SHORT).show();
+                                         //   Toast.makeText(ProfileActivity.this,"Request Sent",Toast.LENGTH_SHORT).show();
                                         }
                                     }
                                 });
@@ -118,6 +162,42 @@ public class ProfileActivity extends AppCompatActivity {
                         }
                     });
                 }
+
+                //---------------- CANCEL FRIEND REQUEST STATE -------------------
+
+                    if(mState.equals("request_sent"))
+                    {
+                        mFriendRequestDatabase.child(mCurrent_User.getUid()).child(user_Id).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful())
+                                     {
+                                          mFriendRequestDatabase.child(user_Id).child(mCurrent_User.getUid()).removeValue().addOnCompleteListener(new OnCompleteListener<Void>() {
+                                          @Override
+                                          public void onComplete(@NonNull Task<Void> task) {
+
+                                              if (task.isSuccessful())
+                                              {
+                                                  mProfileSendReqButton.setEnabled(true);
+                                                  mState = "not_friends";
+                                                  mProfileSendReqButton.setText("SEND FRIEND REQUEST");
+                                              }
+                                              else
+                                              {
+
+                                              }
+
+                                                }
+                                           });
+                                     }
+                                else
+                                {
+
+                                }
+                            }
+                        });
+                    }
+
             }
         });
     }
@@ -125,7 +205,8 @@ public class ProfileActivity extends AppCompatActivity {
 
 
 
-    //---------------- CANCEL FRIEND REQUEST STATE -------------------
+
+
 
 
 }
